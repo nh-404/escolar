7199136f484be5c7ff89453c6bd4b341802fb495
Tasnimul Hasan <63117803+nh-404@users.noreply.github.com>
2024-08-18 01:44:27 +0600

    Update urls.py

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,13 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path
-from teacher import views
+from teachers import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   
     path('teacher', views.teacherDashboard, name='teacher'),
     path('teacherlist', views.teacherList, name='teacherList'),
     path('teacherData', views.teacherData, name='teacherData'),
-    path('edit/<int:id>/', views.teacherEdit, name='teacherEdit'),  
-    path('remove/<int:id>/', views.remove, name='remove'),  # Ensure <int:id> is used
+    path('edit/<int:id>/', views.teacher_Edit, name='teacherEdit'),  
+    path('remove/<int:id>/', views.teacher_Remove, name='teacherRemove'),  # Ensure <int:id> is used
 ]
